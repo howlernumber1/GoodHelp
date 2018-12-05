6d6291fbc368cbcb88eb8b2ac00966dc9fa856e7
Ashley Donald <ashd32@gmail.com>
2018-12-05 14:20:48 -0500

    configured keys

--- server.js
+++ server.js
@@ -1,38 +1,11 @@
-const express = require("express");
+const express = require('express');
+const mongoose = require('mongoose');
 
-const mongoose = require("mongoose");
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
 const app = express();
-const PORT = process.env.PORT || 3001;
-
-// Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
-
-// -----------------Database configuration with Mongoose---------------
-// -----------------Define local MongoDB URI---------------
-var databaseUri = 'mongodb://localhost/goodhelpApp';
-//------------------------------------------------
-if (process.env.MONGODB_URI) {
-//THIS EXECUTES IF THIS IS BEING EXECUTED IN YOUR HEROKU APP
-  mongoose.connect(process.env.MONGODB_URI);
-} else {
-//THIS EXECUTES IF THIS IS BEING EXECUTED ON YOUR LOCAL MACHINE
-  mongoose.connect(databaseUri);
-}
-//-----------------End database configuration-------------------------
-
-var db = mongoose.connection;
-
-// show any mongoose errors
-db.on('error', function(err) {
-  console.log('Mongoose Error: ', err);
-})
-
 
 // DB Config
 const db = require('./config/keys').mongoURI;
@@ -51,12 +24,7 @@ app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
 
-//once logged in to the db through mongosse, log a success message
-db.once('open', function() {
-  console.log('Mongoose connection sucessful.');
-})
+// required for Heroku
+const port = process.env.PORT || 5000;
 
-// Start the API server
-app.listen(PORT, function() {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
-});
+app.listen(port, () => console.log(`Server running on port ${port}`));
