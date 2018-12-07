535142ebc875170cdd75efeff83fa2f0d022e21c
Ashley Donald <ashd32@gmail.com>
2018-12-07 12:01:39 -0500

    corrected route location

--- models/Client.js
+++ models/Client.js
@@ -1,36 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
-const passport = require('passport');
+const Schema = mongoose.Schema;
 
-// Load Customer model
-const customer = require('../../models/Customer');
-
-// Load ServiceProvider model 
-const serviceprovider = require('../../models/ServiceProvider');
-
-// Load Client model
-const client = require('../../models/Client');
-
-// @route   GET  api/profile/test
-// @desc    TESTS profile route
-// @access  PUBLIC
-
-router.get('/test', (req, res) => res.json({msg: 'Profile Works!'}));
-
-// @route   GET  api/profile
-// @desc    GET current users profile
-// @access  PRIVATE
-router.get('/', passport.authenticate('jwt', {session: false}), (req, res) => {
-    Customer.findOne({ user: req.user.id })
-        .then(profile => {
-            if(!profile) {
-                errors.noprofile = 'There is no profile for this user!';
-                return res.status(400).json(errors);
-            }
-        res.json(profile);
-        })
-        .catch(err => res.status(404).json(err));
+var ClientSchema = new Schema ({
+    name: String,
+    phone: String,
+    email: String
 });
 
-module.exports = router;
+const Client = mongoose.model('Client', ClientSchema);
+
+module.exports = Client;
