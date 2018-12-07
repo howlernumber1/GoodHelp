7b09766d9b5815393ca6b3aed5c812c74526fd9a
Ashley Donald <ashd32@gmail.com>
2018-12-07 11:49:27 -0500

    makes customer profile routes

--- models/Client.js
+++ models/Client.js
@@ -1,12 +1,36 @@
+const express = require('express');
+const router = express.Router();
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const passport = require('passport');
 
-var ClientSchema = new Schema ({
-    name: String,
-    phone: String,
-    email: String
+// Load Customer model
+const customer = require('../../models/Customer');
+
+// Load ServiceProvider model 
+const serviceprovider = require('../../models/ServiceProvider');
+
+// Load Client model
+const client = require('../../models/Client');
+
+// @route   GET  api/profile/test
+// @desc    TESTS profile route
+// @access  PUBLIC
+
+router.get('/test', (req, res) => res.json({msg: 'Profile Works!'}));
+
+// @route   GET  api/profile
+// @desc    GET current users profile
+// @access  PRIVATE
+router.get('/', passport.authenticate('jwt', {session: false}), (req, res) => {
+    Customer.findOne({ user: req.user.id })
+        .then(profile => {
+            if(!profile) {
+                errors.noprofile = 'There is no profile for this user!';
+                return res.status(400).json(errors);
+            }
+        res.json(profile);
+        })
+        .catch(err => res.status(404).json(err));
 });
 
-const Client = mongoose.model('Client', ClientSchema);
-
-module.exports = Client;
+module.exports = router;
