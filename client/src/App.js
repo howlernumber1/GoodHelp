b6b65ed8c3fd0fde330853e0d643bee67e5811db
Calithea <tiffanyhall87@gmail.com>
2018-12-07 11:34:41 -0500

    made changes to homepage UI and added functionality to buttons on homepage provided by michael

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
 import HomePage from './components/HomePage/HomePage';
-// import SearchPage from "./components/SearchPage/SearchPage";
-import ServicesPage from "./components/ServicesPage/ServicesPage";
 import UserPage from "./components/UserPage/UserPage";
 import ProviderPage from "./components/ProviderProfile/ProviderProfile";
 import NoMatch from "./components/NoMatch/NoMatch";
@@ -13,34 +11,33 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-    <BrowserRouter>
-    <div>
-      <header>
-        <nav className="navbar d-flex justify-content-end">
-        <p className="navbar-brand"> GoodHelp</p>
-         <a className="links"> <Link to={'/'}>Home</Link> </a> | 
+      <BrowserRouter>
+        <div>
+          <header>
+            <nav className="navbar">
+              <p className="navbar-brand"> GoodHelp</p>
+              <span>
+                <a className="links"> <Link to={'/'}>Home</Link> </a> |
           {/* <Link to={'/search'}>Search</Link> | */}
-          <a className="links"> <Link to={'/services'}>Services</Link> </a>|
+                <a className="links"> <Link to={'/services'}>Services</Link> </a>|
           <a className="links"><Link to={'/user'}>Member</Link> </a>|
           <a className="links"><Link to={'/user'}>Provider</Link> </a>
+              </span>
 
+            </nav>
+          </header>
 
-        </nav>
-      </header>
+          <Switch>
+            <Route exact path='/' component={HomePage} />
+            <Route exact path='/user' component={UserPage} />
+            <Route exact path='/provider' component={ProviderPage} />
+            <Route component={NoMatch} />
+          </Switch>
 
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        {/* <Route exact path='/search' component={SearchPage} /> */}
-        <Route exact path='/services' component={ServicesPage} />
-        <Route exact path='/user' component={UserPage} />
-        <Route exact path='/provider' component={ProviderPage} />
-        <Route component={NoMatch} />
-      </Switch>
-
-    </div>
-  </BrowserRouter>
-);
-}
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
--- client/src/components/HomePage/HomePage.js
+++ client/src/components/HomePage/HomePage.js
@@ -1,166 +1,113 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Category from '../Categories/Category';
 
-const HomePage = () => (
+class HomePage extends Component {
 
-  <div className="wrapper">
+  state = {
+    providerSearchInput: ''
+  }
 
-{/* home section */}
+  handleProviderSearchChange = (event) => (
+    this.setState(
+      {
+        providerSearchInput: event.target.value
+      }
+    )
+  )
 
-<div className="jumbotron">
+  handleProviderSearchClick = (event) => {
+    event.preventDefault();
+    console.log(this.state.providerSearchInput)
+  }
 
-<h1 className="display-4">Welcome to <span class="title">GoodHelp</span></h1>
-<p className="lead">They say "Good Help" is hard to find...well, not anymore. </p>
-<hr className="my-4"/>
-<p>Search within our large list of credited and reliable businesses to find the right one for your property management needs.</p>
-<p>If you are a small business who wants to join 'Good Help', sign up now as a provider and grow your clientele (or business).</p>
-<a className="btn btn-lg" href="#" role="button">Get Started</a>
+  handleServiceClick = (event) => {
+    event.preventDefault();
+    console.log(event.target.value)
+  }
 
- </div>
+  render() {
 
+    return (
+      <div className="wrapper">
 
-{/* search section */}
+        {/* home section */}
 
-<div className="searchSection">  
-    <h4>Looking for a specific service provider?</h4>
-    <p>Search here:</p>
-    <form className="form-inline">
-  <input className="form-control mr-sm-2" type="search" placeholder="Provider Name" aria-label="Search" />
-  <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-</form>
+        <div className="jumbotron jumbotron-fluid">
 
- <br />
+          <h1 className="display-4">Welcome to <span class="title">GoodHelp</span></h1>
+          <p className="lead">They say "Good Help" is hard to find...well, not anymore. </p>
+          <hr className="my-4" />
+          <p>Search within our large list of credited and reliable businesses to find the right one for your property management needs.</p>
+          <p>If you are a small business who wants to join 'Good Help', sign up now as a provider and grow your clientele (or business).</p>
+          <div className="d-flex justify-content-center">
+            <a className="btn btn-lg" href="#" role="button">Get Started</a>
+          </div>
 
-<div className="results">
- <p> Research Results Append Here </p>
-  </div>
-
-</div>
-
-<hr />
-
-{/* Services Section */}
-
-  <div className="container">
-    <br />
-    <div className="card-deck row">
-      <div className="card col-2">
-        <img className="card-img-top img-fluid" src="./images/auto.png" alt="broken down blue car" />
-        <div className="card-body">
-          <h5 className="card-title">Auto Services</h5>
-          <a href="auto" className="btn btn-primary iconBtn">View Businesses</a>
         </div>
-      </div>
-      <div className="card col-3">
-        <img className="card-img-top" src=".../100px200/" alt="assembly" />
-        <div className="card-body">
-          <h5 className="card-title">Assembly Services</h5>
-          <a href="assembly" className="btn btn-primary iconBtn">View Businesses</a>
-        </div>
-      </div>
-      <div className="card col-3">
-        <img className="card-img-top img-fluid" src=".../100px200/" alt="boat" />
-        <div className="card-body">
-          <h5 className="card-title">Boat Services</h5>
-          <a href="boat" className="btn btn-primary iconBtn">View Businesses</a>
-        </div>
-      </div>
-   
-    <br />
-    
-      <div className="card col-3">
-        <img className="card-img-top img-fluid" src=".../100px200/" alt="housekeeping" />
-        <div className="card-body">
-          <h5 className="card-title">Cleaning & Housekeeping Services</h5>
-          <a href="housekeeping" className="btn btn-primary iconBtn">View Businesses</a>
-        </div>
-      </div>
-      </div>
-      <div className="card-deck row">
-      <div className="card col-4">
-        <img className="card-img-top img-fluid" src=".../100px200/" alt="delivery" />
-        <div className="card-body">
-          <h5 className="card-title">Delivery & Pick-Up Services</h5>
-          <a href="delivery" className="btn btn-primary iconBtn">View Businesses</a>
-        </div>
-      </div>
-      <div className="card col-4">
-        <img className="card-img-top img-fluid" src=".../100px200/" alt="light bulb" />
-        <div className="card-body">
-          <h5 className="card-title">Electrical Services</h5>
-          <a href="electical" className="btn btn-primary iconBtn">View Businesses</a>
-        </div>
-      </div>
-    </div>
-    <br />
-    <div className="card-deck row">
-      <div className="card col-4">
-        <img className="card-img-top img-fluid" src=".../100px200/" alt="hvac system" />
-        <div className="card-body">
-          <h5 className="card-title">HVAC Services</h5>
-          <a href="hvac" className="btn btn-primary iconBtn">View Businesses</a>
-        </div>
-      </div>
-      <div className="card col-4">
-        <img className="card-img-top img-fluid" src=".../100px200/" alt="lawnmower" />
-        <div className="card-body">
-          <h5 className="card-title">Lawn & Landscaping Services</h5>
-          <a href="lawn" className="btn btn-primary iconBtn">View Businesses</a>
-        </div>
-      </div>
-      <div className="card col-4">
-        <img className="card-img-top img-fluid" src=".../100px200/" alt="flat screen tv" />
-        <div className="card-body">
-          <h5 className="card-title">Mounting & Installation Services</h5>
-          <a href="mount" className="btn btn-primary iconBtn">View Businesses</a>
-        </div>
-      </div>
-    </div>
-    <br />
-    <div className="card-deck row">
-      <div className="card col-4">
-        <img className="card-img-top img-fluid" src=".../100px200/" alt="moving boxes" />
-        <div className="card-body">
-          <h5 className="card-title">Moving Services</h5>
-          <a href="move" className="btn btn-primary iconBtn">View Businesses</a>
-        </div>
-      </div>
-      <div className="card col-4">
-        <img className="card-img-top img-fluid" src=".../100px200/" alt="organized and color coded files" />
-        <div className="card-body">
-          <h5 className="card-title">Organization Services</h5>
-          <a href="organize" className="btn btn-primary iconBtn">View Businesses</a>
-        </div>
-      </div>
-      <div className="card col-4">
-        <img className="card-img-top img-fluid" src=".../100px200/" alt="paint roller" />
-        <div className="card-body">
-          <h5 className="card-title">Painting Services</h5>
-          <a href="paint" className="btn btn-primary iconBtn">View Businesses</a>
-        </div>
-      </div>
-    </div>
-    <br />
-    <div className="card-deck row">
-      <div className="card col-4">
-        <img className="card-img-top img-fluid" src=".../100px200/" alt="leaky faucet" />
-        <div className="card-body">
-          <h5 className="card-title">Plumbing Services</h5>
-          <a href="plumbing" className="btn btn-primary iconBtn">View Businesses</a>
-        </div>
-      </div>
-    </div>
-  </div>
 
-  {/* footer */}
-<footer className="footer">
-<div >
-  <span>Copyright @ 2018</span>
-  </div>
-  </footer>
 
-  </div>
+        {/* search section */}
 
-)
+        <div className="container">
+          <div className="row d-flex justify-content-center">
+            <div className="searchSection">
+              <h4>Looking for a specific service provider?</h4>
+              <br />
+              <form className="form-inline d-flex justify-content-center">
+                <input onChange={this.handleProviderSearchChange} value={this.state.providerSearchInput} className="form-control mr-sm-2" type="search" placeholder="Provider Name" aria-label="Search" />
+                <button onClick={this.handleProviderSearchClick} className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+              </form>
+            </div>
+          </div>
+          <br />
+          <div className="row results d-flex justify-content-center">
+          </div>
+
+        </div>
+
+        <hr />
+
+        {/* Services Section */}
+
+        <div className="container">
+          <br />
+          <div className="card-deck row">
+            <Category handleServiceClick={this.handleServiceClick} category="Auto Services" img="./images/auto.png" alt="broken down blue car" />
+            <Category handleServiceClick={this.handleServiceClick} category="Assembly Services" img="./images/tools.png" alt="wooden tool box full of tools" />
+            <Category category="Boat Services" img="./images/Boat-1.png" alt="white and gray speed boat with a blue canopy" />
+            <Category category="Cleaning &amp; Housekeeping Services" img="./images/vacuum.png" alt="blue vacuum" />
+          </div>
+          <br />
+          <div className="card-deck row">
+            <Category handleServiceClick={this.handleServiceClick} category="Delivery &amp; Pick-Up/Moving Services" img="./images/organize.png" alt="loaded boxes in the back of a white moving truck" />
+            <Category handleServiceClick={this.handleServiceClick} category="Electrical Services" img="./images/bulb.png" />
+            <Category handleServiceClick={this.handleServiceClick} category="HVAC Services" img="./images/ac.png" alt="gray hvac unit" />
+            <Category handleServiceClick={this.handleServiceClick} category="Landscaping &amp; Lawn Services" img="./images/mower.png" alt="green and yellow riding lawnmower" />
+          </div>
+          <br />
+          <div className="card-deck row">
+            <Category handleServiceClick={this.handleServiceClick} category="Mounting &amp; Installation Services" img="./images/tv3.png" alt="flat screen tv" />
+            <Category handleServiceClick={this.handleServiceClick} category="Organization Services" img="./images/files.png" alt="daily planner with color dividers" />
+            <Category handleServiceClick={this.handleServiceClick} category="Painting Services" img="./images/paint.png" />
+            <Category handleServiceClick={this.handleServiceClick} category="Plumbing Services" img="./images/sink.png" alt="white toilet" />
+          </div>
+          <br />
+          <a href="#">Back to Top</a>
+
+        </div>
+
+        {/* footer */}
+        <footer className="footer">
+          <div >
+            <span>Copyright @ 2018</span>
+          </div>
+        </footer>
+
+      </div>
+
+    );
+  }
+}
 
 
 export default HomePage;
