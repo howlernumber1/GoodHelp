1df132dd295e45cb2159c0c2e215a06600bacf08
Ashley Donald <ashd32@gmail.com>
2018-12-12 23:56:31 -0500

    adds request toggle button

--- client/src/components/Services/Services.js
+++ client/src/components/Services/Services.js
@@ -36,11 +36,6 @@ const service = (props) => (
                     <div className="col-md-12">
                         {/* <div className="card card-body bg-info text-white mb-3"> */}
                         <div className="card card-body bg-Light text-black mb-3">
-                            <div className="row">
-                                <div className="col-4 col-md-3 m-auto">
-                                    <img className="rounded-circle" src="//www.gravatar.com/avatar/8cb7902f0a6c100e16cd43f97c0cf0f7?s=200&amp;rating=pg&amp;default=mm" alt=""></img>
-                                </div>
-                            </div>
                             <div className="text-center">
                                 <h1 className="display-4 text-center">Services</h1>
                                 <p></p>
