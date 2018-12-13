412317e12e1512e022e2dfe63e02b225b46b8b69
Kora25 <sarekabelnavis@gmail.com>
2018-12-12 21:06:24 -0500

    Update provider and user pages

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const passport = require('passport');
-const path = require('path');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const path = require("path");
 
-const users = require('./routes/api/users');
-const profile = require('./routes/api/profile');
-const services = require('./routes/api/services');
-const post = require('./routes/api/posts');
-const providers = require('./routes/api/ServiceProvider')
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const services = require("./routes/api/services");
+const post = require("./routes/api/posts");
+const providers = require("./routes/api/ServiceProvider");
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 3000;
 
 // Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -22,37 +22,37 @@ app.use(bodyParser.json());
 app.use(passport.initialize());
 
 // Passport Config
-require('./config/passport')(passport);
+require("./config/passport")(passport);
 
 // Use Routes
-app.use('/api/users', users);
-app.use('/api/profile', profile);
-app.use('/api/services', services);
-app.use('/api/post', post);
-app.use('/api/serviceprovider', providers);
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/services", services);
+app.use("/api/post", post);
+app.use("/api/serviceprovider", providers);
 
 // DB Config
 // const db = require('./config/keys').mongoURI;
 
 // Server static assets if in production
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static("client/build"));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
 // -----------------Database configuration with Mongoose---------------
 // -----------------Define local MongoDB URI---------------
-var databaseUri = 'mongodb://localhost/goodhelp';
+var databaseUri = "mongodb://localhost/goodhelp";
 //------------------------------------------------
 if (process.env.MONGODB_URI) {
-//THIS EXECUTES IF THIS IS BEING EXECUTED IN YOUR HEROKU APP
+  //THIS EXECUTES IF THIS IS BEING EXECUTED IN YOUR HEROKU APP
   mongoose.connect(process.env.MONGODB_URI);
 } else {
-//THIS EXECUTES IF THIS IS BEING EXECUTED ON YOUR LOCAL MACHINE
+  //THIS EXECUTES IF THIS IS BEING EXECUTED ON YOUR LOCAL MACHINE
   mongoose.connect(databaseUri);
 }
 //-----------------End database configuration-------------------------
@@ -68,16 +68,14 @@ var db = mongoose.connection;
 //   .catch(err => console.log(err));
 
 // show any mongoose errors
-db.on('error', function(err) {
-  console.log('Mongoose Error: ', err);
-})
-
+db.on("error", function(err) {
+  console.log("Mongoose Error: ", err);
+});
 
 //once logged in to the db through mongosse, log a success message
-db.once('open', function() {
-  console.log('Mongoose connection sucessful.');
-})
-
+db.once("open", function() {
+  console.log("Mongoose connection sucessful.");
+});
 
 // Start the API server
 app.listen(PORT, function() {
